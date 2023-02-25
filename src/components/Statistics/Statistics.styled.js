0d3ec656312716429659cc047afe62783fdd2f49
OlesiaMelnyk1 <elsonidodelmar26@gmail.com>
2023-02-25 19:42:09 +0200

    edit feedback

--- src/components/Statistics/Statistics.styled.js
+++ src/components/Statistics/Statistics.styled.js
@@ -4,9 +4,9 @@ export const StatisticsList = styled.ul`
   display: flex;
   flex-direction: column;
   gap: 12px;
+  align-items: flex-start;
   margin: 0;
   padding: 0;
   font-size: 16px;
   list-style: none;
-  align-items: flex-start;
 `;
